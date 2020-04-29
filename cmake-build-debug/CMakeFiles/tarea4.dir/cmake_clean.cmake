7f731457f023bfcb6064059a8bfa6849f0573986
Ricardo-1998 <00004817@uca.edu.sv>
2020-04-29 16:44:25 -0600

    tarea4

---
+++ cmake-build-debug/CMakeFiles/tarea4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tarea4.dir/main.cpp.obj"
+  "libtarea4.dll.a"
+  "tarea4.exe"
+  "tarea4.exe.manifest"
+  "tarea4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tarea4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
